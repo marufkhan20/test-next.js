07f9bc5eef0660719057ccb5cc404f540181ee47
Rashedul Islam Maruf <54831756+marufkhan20@users.noreply.github.com>
2025-05-21 16:24:11 +0600

    Update route.ts

--- app/api/test/[[...slug]]/route.ts
+++ app/api/test/[[...slug]]/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-  req: NextRequest,
-  context: { params: { slug?: string[] } }
 ) {
-  return NextResponse.json({ slug: context.params.slug || [] });
+  return NextResponse.json({ message: "Hello" });
 }
